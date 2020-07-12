809e8d8d256e9e948df0ced29996cb269144db3c
Mathews Motta <mathewscsmotta@gmail.com>
2020-07-12 17:32:11 +0100

    Adicionada primeira versão

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+
+    [SerializeField] private RectTransform _transform;
+    [SerializeField] private Canvas _canvas;
+    [SerializeField] private CanvasGroup _canvasGroup;
+
+    [SerializeField] private AudioSource _audioSource;
+
+    [SerializeField] private AudioClip _grab;
+    [SerializeField] private AudioClip _drop;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _canvasGroup.alpha = 0.5f;
+        _canvasGroup.blocksRaycasts = false;
+        _audioSource.clip = _grab;
+        _audioSource.Play();
+    }
+
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _canvasGroup.alpha = 1f;
+        _canvasGroup.blocksRaycasts = true; 
+        _audioSource.clip = _drop;
+        _audioSource.Play();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _transform.anchoredPosition += eventData.delta / _canvas.scaleFactor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Apertou!");
+    }
+}
